785ae8ad439d322b455f8229822cd6224c87ca48
Kece05 <137594110+Kece05@users.noreply.github.com>
2023-12-12 10:21:32 -0500

    asdf
    asd

--- Data-ChainV2/frontend/react-app/src/components/WalletDash/encrypt.js
+++
@@ -1,8 +0,0 @@
-import CryptoJS from "crypto-js";
-
-function encrypt (text, key) {
-  var encrypted = CryptoJS.AES.encrypt(text, key);
-  return encrypted.toString();
-}
-
-export default encrypt;
--- Data-ChainV2/frontend/react-app/src/components/createToken/testValue.js
+++
@@ -1,8 +0,0 @@
-function isInteger(value) {
-    if(parseInt(value,10).toString()===value) {
-      return true
-    }
-    return false;
-  }
-  
-export default isInteger;
